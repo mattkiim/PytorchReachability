b64958df3033df03741ea88ae12531148b3d00a9
mattkiim <kimmatthew0604@gmail.com>
2025-07-16 17:08:34 -0400

    change

--- dreamerv3-torch/tools.py
+++ dreamerv3-torch/tools.py
@@ -260,7 +260,7 @@ def fill_expert_dataset_dubins(config, cache, is_val_set=False):
             if config.heat_mode < 2:
                 heat_failure = heat
             elif config.heat_mode == 2 or config.heat_mode == 3:
-                heat_failure = heat > 1 - (config.heat_threshold - 1e-6)
+                heat_failure = heat < 1 - (config.heat_threshold - 1e-6)
             # print(f"[tools/fill_expert_dataset_dubins] heat check: {heat}"); quit()
 
             transition["failure"] = vis_failure
