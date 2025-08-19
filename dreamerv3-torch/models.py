6c1c7f2b7e7f863a64b8b437875091edd9d2ef0a
mattkiim <kimmatthew0604@gmail.com>
2025-08-19 04:05:26 -0400

    working non-actuated

--- dreamerv3-torch/models.py
+++ dreamerv3-torch/models.py
@@ -239,6 +239,8 @@ class WorldModel(nn.Module):
             for k, v in obs.items()
         }
         
+        # obs["image"] = obs["image"] / 255.0
+        
         if "discount" in obs:
             obs["discount"] *= self._config.discount
             # (batch_size, batch_length) -> (batch_size, batch_length, 1)
